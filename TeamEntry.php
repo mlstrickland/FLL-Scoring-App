300d89c4bc46c230faf51ab101a641884cf5a2e6
Mike <mlstrickland42@gmail>
2015-09-20 10:36:27 -0400

    Team Entry Rev2
    Now with 100% less depricated code and 100% more documentation!

--- TeamEntry.php
+++ TeamEntry.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML> 
 <html>
 <head>
+<!--color for error messages--> 
 <style>
 .error {color: #FF0000;}
 </style>
@@ -8,82 +9,69 @@
 <body> 
 
 <?php
-$name = "";
-$number = 0;
 
+
+//error flag
 $error = 0;
 
+//variables for database connection
 $hostname = 'localhost';
-	
 $username = 'root';
-	
 $password = 'password';
-
-function printStuff(){
- print "stuff";
-}
 
 // define variables and set to empty values
 $nameErr = $numberErr = $genderErr = $websiteErr = "";
+//values initialized here will show up in the textboxes when the page first loads
 $name = $number = "";
 
+//if ADD button is pressed
 if (isset($_POST["ADD"])) {
-	
 	if (empty($_POST["name"])) {
      $nameErr = "Name is required";
 	 $error = 1;
-
    }
    else {
 	   $name = test_input($_POST["name"]);
    }
-
+	//if textbox is empty display error message
    if (empty($_POST["number"])) {
      $numberErr = "Number is required";
 	 $error = 1;
+	//if textbox does not only contains numbers display error message
    } else if (is_numeric($_POST["number"]) != 1){
 	 $numberErr = "Team number can only contain numbers";
 	 $error = 1;
-	 
-	 
 	}
-     // check if name only contains letters and whitespace
-   //else if (!is_numeric($number)) {
-	//	$numberErr = "Only numbers allowed"; 
-   //}
-	else{
-		 $number = test_input($_POST["number"]);
-	 }
-	if ($error != 1){
+	//if no error set the variable to the value in the box and add the data to the database
+	else{ 
+	$number = test_input($_POST["number"]);
+	//if ($error != 1){
 		try{ 
 			$dbh = new PDO("mysql:host=$hostname;dbname=teamlist", $username);
-			
-			//$dbh->query("INSERT INTO `schedule` ('Round', 'Table1', 'Table2`, `Table3`, `Table4`) VALUES ('$MacthNum','$Team1','$Team2','$Team3','$Team4');");	
 			$dbh->query("INSERT INTO `teams`(`TeamNumber`, `Score1`, `score2`, `score3`, `score4`, `score5`, `highscore`, `TeamName`) VALUES ('$number',0,0,0,0,0,0,'$name')");	
-			
 			}
 		catch(PDOException $e){
 			echo $e->getMessage();
 		}
 	}
 }
+//if REMOVE button is pressed
 if (isset($_POST["REMOVE"])) {
+	//get team name from dropdown
 	$Team = $_POST['Team'];
+	//remove team from database
 	try{
 		$dbh = new PDO("mysql:host=$hostname;dbname=teamlist", $username);
 		$dbh->query("DELETE FROM `teams` WHERE `TeamNumber` = $Team");	
-		//$dbh->query("DELETE FROM `schedule` WHERE `Round` = $MacthNum AND `Table1` = $Team1 AND `Table2` = $Team2 AND `Table3` = $Team3 AND `Table4` = $Team4;");	
+		
 	}
 	catch(PDOException $e){
 		echo $e->getMessage();
-	}
-		//$query = "INSERT INTO `teamlist` (`TeamNumber`, `TeamName`) VALUES ('$number','$name')";	
-		//$query = "INSERT INTO `teamlist` (`TeamNumber`,`Score1`, `score2`,`score3`,`score4`,`score5`,`highscore`, `rank`, `TeamName`) VALUES ('$number',0,0,0,0,0,0,'$name')";	
-		//$query = "INSERT INTO `teams`(`TeamNumber`, `Score1`, `score2`, `score3`, `score4`, `score5`, `highscore`, `TeamName`) VALUES ('$number',0,0,0,0,0,0,'$name')";	
+	}	
 }
 	
 
-
+//remove spaces and slashes and convert to html entities
 function test_input($data) {
    $data = trim($data);
    $data = stripslashes($data);
@@ -93,63 +81,47 @@ function test_input($data) {
 ?>
 
 <h2>Team Entry</h2>
-<p><span class="error">* required field.</span></p>
 <form method="post" action=""> 
-   Team Name <input type="text" name="name" value="<?php echo $name;?>">
-   <span class="error">* <?php echo $nameErr;?></span>
-   <br><br>
-   Team Number: <input type="text" name="number" value="<?php echo $number;?>">
-   <span class="error">* <?php echo $numberErr;?></span>
-   <br><br>
-   <input type="submit" name="ADD" value="ADD">
-   <br>
-	<!--<input type="radio" name="MODE" id="INSERT" value="INSERT" <label for="INSERT">INSERT</label>!-->
-	<!--<input type="radio" name="MODE" id="UPDATE" value="UPDATE" <label for="UPDATE">UPDATE</label>!--> <br> 
+	<!-- team name field for user input-->
+	Team Name <input type="text" name="name" value="<?php echo $name;?>">
+	<span class="error"> <?php echo $nameErr;?></span>
+	<br><br>
+	<!-- team number field for user input-->
+	Team Number: <input type="text" name="number" value="<?php echo $number;?>">
+	<span class="error"> <?php echo $numberErr;?></span>
+	<br><br>
+	<input type="submit" name="ADD" value="ADD">
+	<br>
+	<br> 
 
- 
-<?php
-	
+	<?php
+	//get current team data from database
 	try{ 
 		$dbh = new PDO("mysql:host=$hostname;dbname=teamlist", $username);
+		//print if connection was successful
 		//echo "<P> Connected to Database<br />";
 		$sql = "SELECT * FROM teams ORDER BY TeamNumber";
 		
-				echo '<select name="Team">';
-				//<?php 
-				//$sql = mysql_query("SELECT TeamName AND TeamNumber FROM users");
-					echo "<option value=\"Team\">" . 'Select Team' . "</option>";
-				foreach($dbh->query($sql) as $row){
-					echo "<option value=\"" . $row['TeamNumber'] . "\">" . $row['TeamNumber'] . " - " . $row['TeamName'] . "</option>";
-				}
-				
-				echo '</select>';
-				?>
-				
-				
-				<?php
-		//echo "</table>";
-		//print $row['TeamNumber']. "-" . $row['TeamName'] . " " . $row['Score1'] . " " . $row['Score2'] . " " . $row['Score3'] . " " . $row['Score4'] . " " . $row['Score5'] . " " . $row['HighScore'] . " " . $row['Rank'] . " " . '<br />';
-
+		//place team data in dropdown		
+		echo '<select name="Team">';
+		echo "<option value=\"Team\">" . 'Select Team to Remove' . "</option>";
+		foreach($dbh->query($sql) as $row){
+			echo "<option value=\"" . $row['TeamNumber'] . "\">" . $row['TeamNumber'] . " - " . $row['TeamName'] . "</option>";
+		}
 		
+		echo '</select>';
 	}
 	catch(PDOException $e){
 		echo "Failed to connect <br>";
 		echo $e->getMessage();
 	}
-	?>
-	<br><br>
-	<!--<input type="radio" name="MODE" id="REMOVE" value="REMOVE" <label for="REMOVE">REMOVE</label>!-->
-	<input type="submit" name="REMOVE" value="REMOVE">
-	<?php
 	
+	echo "<br><br>";
+	//remove button for user input
+	echo "<input type='submit' name='REMOVE' value='REMOVE'>";
+	//close the connection
 	$dbh = null;
-	
-
-?>
-	</P>
-	
-</p>
-   
+	?>
 </form>
 
 </body>
